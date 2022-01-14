cf5c7ee37107d28379b9b5cdece9493a2d4bd571
pharaohs-son <60492463+pharaohs-son@users.noreply.github.com>
2022-01-14 18:47:32 -0300

    Update main_teste.py

--- scripts/main_teste.py
+++ scripts/main_teste.py
@@ -1,13 +1,13 @@
 #!/python3
 import os
 import pkgutil
+import sys
 
-os.system("echo 'teste'")
-pt = os.path.dirname(os.path.realpath(__file__))
-config = os.path.join(pt, 'config/config.config')
+file_path = os.path.join(sys.prefix, 'config.config')
+
 print(config)
 
-with open(coonfig, "r") as conf:
+with open(config, "r") as conf:
   print(conf.read())
   
 
